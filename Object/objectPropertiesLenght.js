cf9fdb6d3b5c6da222f428538c30f3905bcb082d
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-17 03:12:56 +0600

    .

---
+++ Object/objectPropertiesLenght.js
@@ -0,0 +1,9 @@
+const obj = {
+  name: "Hasib",
+  age: 22,
+  profession: "developer",
+};
+
+const numberOfProperties = Object.keys(obj).length;
+
+console.log(numberOfProperties);
---
+++ Object/test.js
@@ -0,0 +1,52 @@
+// function abc() {
+//   console.log(abc.xyz);
+// }
+
+// abc();
+// abc.xyz = 200;
+// abc.xyz = 300;
+// abc();
+// const number = [1, 2, 3, 4];
+// number[100] = 200;
+// console.log(number.length);
+// console.log(typeof typeof 100);
+// const hasib = {
+//   name: "hasib",
+//   age: 32,
+// };
+//--------------------------------------
+// let hasib = [2, 3, 3, 4];
+// const arr = [...hasib];
+// console.log(arr);
+//------------------------------
+// console.log(parseInt("101 + 2"));
+// console.log(parseInt("71FM"));
+// console.log(parseInt("M7F"));
+//------------------------------
+// console.log(
+//   [1, 2].map((num) => {
+//     if (num > 0) return;
+//     return num * 2;
+//   })
+// );
+// console.log(
+//   [1, 2].map((num) => {
+//     if (num > 0) return 0; // Or any other value you want when num > 0
+//     return num * 2;
+//   })
+// );
+//------------------------------------
+// function abc() {
+//   return;
+// }
+
+// console.log(abc());
+//----------------------------------
+"use strict";
+{
+  function a() {
+    console.log("Hasib");
+  }
+}
+
+a();
