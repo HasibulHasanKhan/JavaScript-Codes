5bd6d5d842c4210b2d7aa3a69019fc43b12054ca
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-18 14:20:40 +0600

    .

---
+++ Iteration/array.js
@@ -0,0 +1,14 @@
+// while and do...while
+let numbers = [10, 20, 30, 40, 50];
+let i = 0;
+
+while (i < numbers.length) {
+  console.log(numbers[i]);
+  i++;
+}
+
+let j = 0;
+do {
+  console.log(numbers[j]);
+  j++;
+} while (j < numbers.length);
