b07d6f30f01ab67e282eec67db64d8234e4cd440
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-18 22:57:57 +0600

    .

--- HigherOrderFunction/practice.js
+++ HigherOrderFunction/practice.js
@@ -10,3 +10,40 @@ function fetchData() {
 }
 
 fetchData().then(console.log); // Output after 2s: "Data loaded!"
+
+// Currying for Configurable Functions :
+
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func(...args);
+    } else {
+      return (...nextArgs) => curried(...args, ...nextArgs);
+    }
+  };
+}
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+const curriedAdd = curry(add);
+console.log(curriedAdd(1)(2)(3));
+
+// with bind :
+function curryBind(func, arity = func.length) {
+  return function curried(...args) {
+    if (args.length >= arity) {
+      return func(...args);
+    }
+    return curried.bind(null, ...args);
+  };
+}
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+const curriedAdds = curryBind(add);
+
+console.log(curriedAdds(1)(2)(3));
