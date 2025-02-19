ef4e0a3a0f7b81d1df99f5ed48844675924dbed9
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-19 20:38:07 +0600

    .

---
+++ Map_Set_Array/set.js
@@ -0,0 +1,6 @@
+// const numbers = new Set([1, 2, 3, 4, 5]);
+// console.log(numbers); // Output: Set(5) { 1, 2, 3, 4, 5 }
+
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+const uniqueNumbers = new Set(numbers);
+console.log(uniqueNumbers); // Output: [1, 2, 3, 4, 5]
