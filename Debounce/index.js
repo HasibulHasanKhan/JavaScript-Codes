218e1f104a8a6ee544e0fb28283457cce2518036
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-21 02:40:43 +0600

    .

--- Debounce/index.js
+++ Debounce/index.js
@@ -1,15 +1,22 @@
 const button = document.getElementById("button");
 
-// debounce handler
 function debounce(fn, delay) {
-  setTimeout(() => {
-    fn();
-  }, delay);
+  let timeoutId;
+
+  return function () {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      fn(this);
+    }, delay);
+  };
 }
 
 button.addEventListener(
   "click",
   debounce(function () {
     console.log("clicked");
-  }, 500)
+  }, 1000)
 );
