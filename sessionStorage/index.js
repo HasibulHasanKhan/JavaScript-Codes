fef4c1b9c61429b380b183cbf4fe22d71802d2c8
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-03-26 00:56:05 +0600

    .

---
+++ sessionStorage/index.js
@@ -0,0 +1,28 @@
+function setData(key, value) {
+  sessionStorage.setItem(key, value);
+  alert(`Set ${key} to ${JSON.stringify(value)}`);
+}
+
+function getData(key) {
+  const value = sessionStorage.getItem(key);
+  if (value) {
+    `The value for ${key} is ${value}`;
+  } else {
+    alert(`${key} does not exist in session storage.`);
+  }
+}
+
+function removeData(key) {
+  sessionStorage.removeItem(key);
+  alert(`Removed ${key} from session storage.`);
+}
+
+function clearData() {
+  sessionStorage.clear();
+  alert("Cleared all data from session storage.");
+}
+
+function lengthData() {
+  const length = sessionStorage.length;
+  alert(`There are ${length} items in session storage.`);
+}
